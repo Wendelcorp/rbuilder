7157d5600e06e0d3831a66843cbbc4fc2f808186
wendelcorp <bryce@brycewendelaar.com>
2017-05-07 14:32:52 -0400

    Set up models and resources

---
+++ resume/app/controllers/educations_controller.rb
@@ -0,0 +1,32 @@
+class EducationsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_education
+  end
+
+  def education_params
+  end
+
+end
--- resume/config/routes.rb
+++ resume/config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+
+  resources :educations
+  resources :experiences
+  resources :profiles
+  resources :skills
 end
