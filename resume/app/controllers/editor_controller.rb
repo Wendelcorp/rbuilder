e27503a6200d1acb3c310c57db57d563e100a928
wendelcorp <bryce@brycewendelaar.com>
2017-05-09 00:33:30 -0400

    Built PDF generator functionality

--- resume/app/controllers/editor_controller.rb
+++ resume/app/controllers/editor_controller.rb
@@ -7,5 +7,18 @@ class EditorController < ApplicationController
     @experience = current_user.experiences
     @skill = current_user.skills
 
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf:"Resume",
+        template: "editor/resume.pdf.erb",
+        locals: {
+          @profile => current_user.profiles,
+          @education => current_user.educations,
+          @experience => current_user.experiences,
+          @skill => current_user.skills
+        }
+      end
+    end
   end
 end
