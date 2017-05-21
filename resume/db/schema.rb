0fe4eb8bfffee86115eab188dd129fac89ff6635
wendelcorp <bryce@brycewendelaar.com>
2017-05-21 15:40:52 -0400

    Updated landing page style, added bio to profile model

--- resume/db/schema.rb
+++ resume/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508122246) do
+ActiveRecord::Schema.define(version: 20170521192331) do
 
   create_table "educations", force: :cascade do |t|
     t.string   "program"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170508122246) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.text     "bio"
   end
 
   create_table "skills", force: :cascade do |t|
